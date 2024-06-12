ab1b279a6a81ab02712aefe389dae0c5d0fc02f5
Keisha Zapra <kzapra@gmail.com>
2024-06-12 17:16:34 +0200

    Add guard for missing process when running in a non node environment

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,5 @@
-const DEBUG_ENFLAKE = process.env.DEBUG_ENFLAKE || false;
-export const PERCENT_CHANCE_OF_SUCCESS = +(process.env.PERCENT_CHANCE_OF_SUCCESS || 98);
+const DEBUG_ENFLAKE = process ? process.env?.DEBUG_ENFLAKE || false : false;
+export const PERCENT_CHANCE_OF_SUCCESS = process ? process.env?.PERCENT_CHANCE_OF_SUCCESS || 98 : 98;
 
 export const generatePercentChanceToFlake = () => {
     return Math.floor(Math.random() * 100) + 1
