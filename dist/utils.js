ac7186072117103c59aba4e031d13409b6fa7495
Keisha Zapra <kzapra@gmail.com>
2024-06-12 17:29:40 +0200

    Add guard for missing process when running in a non node environment

--- dist/utils.js
+++ dist/utils.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.log = exports.generatePercentChanceToFlake = exports.PERCENT_CHANCE_OF_SUCCESS = void 0;
-const DEBUG_ENFLAKE = process ? process.env?.DEBUG_ENFLAKE || false : false;
-exports.PERCENT_CHANCE_OF_SUCCESS = process ? +(process.env?.PERCENT_CHANCE_OF_SUCCESS || 98) : 98;
+const DEBUG_ENFLAKE = typeof process !== 'undefined' ? process.env?.DEBUG_ENFLAKE || false : false;
+exports.PERCENT_CHANCE_OF_SUCCESS = typeof process !== 'undefined' ? +(process.env?.PERCENT_CHANCE_OF_SUCCESS || 98) : 98;
 const generatePercentChanceToFlake = () => {
     return Math.floor(Math.random() * 100) + 1;
 };
