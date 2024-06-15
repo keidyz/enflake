cc96bd846191b1cafb25536971d6e878dc733d25
era <era.sundry@gmail.com>
2024-06-15 09:05:58 -0700

    addded FLAKE_CHANCE constant derived from PERCENT_CHANCE_OF_SUCCESS

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,6 @@
 const DEBUG_ENFLAKE = typeof process !== 'undefined' ? process.env?.DEBUG_ENFLAKE || false : false;
 export const PERCENT_CHANCE_OF_SUCCESS = typeof process !== 'undefined' ? +(process.env?.PERCENT_CHANCE_OF_SUCCESS || 98) : 98;
+export const FLAKE_CHANCE = 1 - PERCENT_CHANCE_OF_SUCCESS;
 
 export const generatePercentChanceToFlake = () => {
     return Math.floor(Math.random() * 100) + 1
